0f6f597e476574e18b418d6eb78bd68877c5b50a
Kevin Phon <kevinphon@Kevins-MacBook-Air.local>
2024-01-29 21:49:04 -0800

    search movie input and show movie card

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
 import { useEffect } from "react";
-import './App.css'
-import SearchIcon from './search.svg'
+import "./App.css";
+import SearchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com?apikey=aba8cf59";
 
-const App = () => {
+const movie1 = {
+  "Title": "Batman v Superman: Dawn of Justice (Ultimate Edition)",
+  "Year": "2016",
+  "imdbID": "tt18689424",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BOTRlNWQwM2ItNjkyZC00MGI3LThkYjktZmE5N2FlMzcyNTIyXkEyXkFqcGdeQXVyMTEyNzgwMDUw._V1_SX300.jpg"
+}
 
-  const searchMovies = async(title) => {
-    let response = await fetch(`${API_URL}&s=${title}`)
-    let data = await response.json()
+const App = () => {
+  const searchMovies = async (title) => {
+    let response = await fetch(`${API_URL}&s=${title}`);
+    let data = await response.json();
 
     console.log(data.Search);
-    
-  }
+  };
 
   useEffect(() => {
-    searchMovies('Superman')
-  },[])
+    searchMovies("Superman");
+  }, []);
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    // search movie
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input type="text" placeholder="Superman" onChange={() => {}} />
+
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+
+      {/* movie card  */}
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+
+          <div>
+            <img src={movie1.Poster !== 'N/A' ?  movie1.Poster : 'http://via.placeholder.com/400'} alt={movie1.Title} />
+          </div>
+        </div>
+
+      </div>
+    </div>
   );
 };
 
